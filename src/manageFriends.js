f2d5d2b047b18b88315aedea9704cf2ca28936b3
haider <haiderlek@gmail.com>
2021-08-22 17:05:27 +0300

    commit tree c931af5428d45cb44c4fee997563d737956b0b5e

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,32 @@
 export function manageFriends(state, action) {
-  // your code here
+  switch (action.type) {
+    case "friends/add":
+      return {
+        friends: [...state.friends, action.payload],
+      };
+    case "friends/remove":
+      return {
+        friends: state.friends.filter((friend) => friend.id !== action.payload),
+      };
+    default:
+      return state;
+  }
 }
+manageFriends(
+  { friends: [] },
+  {
+    type: "friends/add",
+    payload: {
+      name: "Chrome Boi",
+      homewtown: "NYC",
+      id: 1,
+    },
+  }
+);
+manageFriends(
+  { friends: [] },
+  {
+    type: "friends/remove",
+    payload: 1,
+  }
+);
--- src/managePresents.js
+++ src/managePresents.js
@@ -1,3 +1,12 @@
 export function managePresents(state, action) {
-  // your code here
+  switch (action.type) {
+    case "presents/increase":
+      return {
+        numberOfPresents: state.numberOfPresents + 1,
+      };
+    default:
+      return {
+        numberOfPresents: 0,
+      };
+  }
 }
